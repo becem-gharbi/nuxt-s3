09e1b420ecc18ba13291e8fc081d6f5fd690e4ec
becem gharbi <becem.gharbi@live.com>
2023-08-16 19:29:02 +0100

    chore: Remove demo

--- demo/server/middleware/auth.ts
+++
@@ -1,9 +0,0 @@
-import { defineEventHandler, getRequestURL } from "#imports";
-
-export default defineEventHandler((event) => {
-  const isS3Mutation = getRequestURL(event).pathname.includes("s3/mutation");
-
-  if (isS3Mutation) {
-    // check authorization
-  }
-});
--- draft.md
+++
@@ -1,4 +0,0 @@
-delete object endpoint/bucket/key
-list object endpoint/bucket
-put object endpoint/bucket/key
-get object endpoint/key
