1fdcc71f8da198aa2388966c85d9ea759fbffa92
becem gharbi <becem.gharbi@live.com>
2023-08-16 13:15:02 +0100

    chore: Add module options

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,57 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import {
+  defineNuxtModule,
+  createResolver,
+  logger,
+  addImportsDir,
+} from "@nuxt/kit";
+import { defu } from "defu";
 
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {
+  accessKeyId: string;
+  secretAccessKey: string;
+  endpoint: string;
+  region: string;
+  bucket: string;
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule'
+    name: "nuxt-s3",
+    configKey: "s3",
   },
-  // Default configuration options of the Nuxt module
-  defaults: {},
-  setup (options, nuxt) {
-    const resolver = createResolver(import.meta.url)
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
-  }
-})
+  setup(options, nuxt) {
+    if (options.accessKeyId) {
+      logger.warn("[nuxt-s3] Please make sure to set accessKeyId");
+    }
+    if (options.endpoint) {
+      logger.warn("[nuxt-s3] Please make sure to set endpoint");
+    }
+    if (options.region) {
+      logger.warn("[nuxt-s3] Please make sure to set region");
+    }
+    if (options.secretAccessKey) {
+      logger.warn("[nuxt-s3] Please make sure to set secretAccessKey");
+    }
+
+    const resolver = createResolver(import.meta.url);
+
+    addImportsDir(resolver.resolve("runtime", "composables"));
+
+    nuxt.options.runtimeConfig = defu(nuxt.options.runtimeConfig, {
+      app: {},
+      s3: {
+        accessKeyId: options.accessKeyId,
+        secretAccessKey: options.secretAccessKey,
+        endpoint: options.endpoint,
+        region: options.region,
+      },
+      public: {
+        s3: {
+          bucket: options.bucket,
+        },
+      },
+    });
+  },
+});
