7d96620035226e09db8f5879b34257c7142868a4
becem-gharbi <becem.gharbi@live.com>
2023-11-14 12:29:23 +0100

    docs: Setup landing page

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,38 +1,25 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: 'Nuxt S3',
+    description: 'Edge compatible S3 client for Nuxt 3',
+    image: '/cover.png',
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
-    },
-    github: {
-      dir: '.starters/default/content',
-      branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
+      github: 'becem-gharbi/nuxt-s3-edge',
     },
     aside: {
       level: 0,
       collapsed: false,
-      exclude: []
+      exclude: [],
     },
     main: {
       padded: true,
-      fluid: true
+      fluid: true,
     },
     header: {
       logo: true,
       showLinkIcon: true,
       exclude: [],
-      fluid: true
-    }
+      fluid: true,
+    },
   }
 })
---
+++ docs/components/Logo.vue
@@ -0,0 +1,8 @@
+<template>
+  <div :style="{ display: 'flex', alignItems: 'center', gap: '8px' }">
+    <img :width="20" src="/logo.png" alt="logo">
+    <h4 :style="{ fontWeight: 'bold' }">
+      Nuxt S3
+    </h4>
+  </div>
+</template>
