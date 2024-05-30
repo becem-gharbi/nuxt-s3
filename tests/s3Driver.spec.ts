97b91757bd7e0abf1245da52ac05cbf9617502f1
becem-gharbi <becem.gharbi@live.com>
2024-05-30 18:21:38 +0100

    test: add `s3` driver tests

---
+++ tests/s3Driver.spec.ts
@@ -0,0 +1,16 @@
+import { describe } from "vitest";
+import s3Driver from "../src/runtime/server/utils/s3Driver";
+import { testDriver } from "./utils";
+
+describe("test s3 driver", () => {
+    testDriver({
+        driver: s3Driver({
+            accessKeyId: process.env.VITE_S3_ACCESS_KEY_ID!,
+            secretAccessKey: process.env.VITE_S3_SECRET_ACCESS_KEY!,
+            accountId: process.env.VITE_S3_ACCOUNT_ID,
+            bucket: process.env.VITE_S3_BUCKET!,
+            endpoint: process.env.VITE_S3_ENDPOINT!,
+            region: process.env.VITE_S3_REGION!
+        })
+    })
+})
